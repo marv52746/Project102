938ca79b8e956712654fd1a29672fea41425ad4c
marv52746 <marv52746@gmail.com>
2024-11-16 21:36:56 +0800

    sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Sidebar, { SidebarItem } from "./components/Sidebar";
+import {
+  LifeBuoy,
+  Receipt,
+  Boxes,
+  Package,
+  UserCircle,
+  BarChart3,
+  LayoutDashboard,
+  Settings,
+} from "lucide-react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sidebar className="w-10">
+        <SidebarItem
+          icon={<LayoutDashboard size={20} />}
+          text="Dashboard"
+          alert
+        />
+        <SidebarItem icon={<BarChart3 size={20} />} text="Statistics" active />
+        <SidebarItem icon={<UserCircle size={20} />} text="Users" />
+        <SidebarItem icon={<Boxes size={20} />} text="Inventory" />
+        <SidebarItem icon={<Package size={20} />} text="Orders" alert />
+        <SidebarItem icon={<Receipt size={20} />} text="Billings" alert />
+        <hr className="my-3" />
+        <SidebarItem icon={<Settings size={20} />} text="Settings" alert />
+        <SidebarItem icon={<LifeBuoy size={20} />} text="Help" alert />
+      </Sidebar>
     </div>
   );
 }
