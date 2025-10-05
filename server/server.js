9b539593e5ded3596f6fa9916ad6df5c3d1188f5
marv52746 <marv52746@gmail.com>
2025-10-05 19:35:27 +0800

    update

--- server/server.js
+++ server/server.js
@@ -16,19 +16,17 @@ connect();
 // ✅ Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(
   cors({
-    origin: "*",
+    origin: ["https://kanvi.net", "http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
-// CORS middleware
-app.use((req, res, next) => {
-  res.setHeader("Cross-Origin-Opener-Policy", "unsafe-none");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+// Handle preflight requests
+app.options("*", cors());
 
 // ✅ Routes
 app.use("/api", require("./router/index"));
